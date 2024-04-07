5aef500b9b540447299e757a558659278f5fa272
Angelotravain <angelotravain96@gmail.com>
2024-04-07 20:05:40 -0300

    Inicio tela de CRID bairros

---
+++ gerapp_front/lib/Modulos/Visoes/Cadastro/Grid/bairro_grid_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BairroGrid extends StatefulWidget {
+  const BairroGrid({super.key});
+
+  @override
+  State<BairroGrid> createState() => _BairroGridState();
+}
+
+class _BairroGridState extends State<BairroGrid> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
